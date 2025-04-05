93fbbb73793051a12e7405cd451f82c3fe1bc25e
Rist <www10177@gmail.com>
2025-04-05 19:19:56 +0800

    init

---
+++ keyboards/open_h11/keymaps/via/keymap.c
@@ -0,0 +1,24 @@
+ #include QMK_KEYBOARD_H
+ void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+ enum layer_names {
+     DJMAX,
+ };
+ 
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+   [DJMAX] = LAYOUT(
+    KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J,
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
+    KC_V, KC_W, KC_X, KC_Y, KC_Z
+  )
+  
+ 
+ 
+ };
+ 
